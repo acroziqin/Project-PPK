26689611a0ee59c8fe317efe6c986d78e1342831
acroziqin <achmadchoirurroziqin@gmail.com>
2017-12-26 10:12:17 +0700

    Tambah Read dan foto
    Telah ditambahkan Read pada bagian member dan ditambahkan fotonya

--- htdocs/project_ppk/jsonMember.php
+++ htdocs/project_ppk/jsonMember.php
@@ -1,9 +1,13 @@
 <?php
   include "db.php";
   $data = array();
-  $q    = mysqli_query($con, "SELECT * FROM `post`");
+  $q    = mysqli_query($con, "SELECT * FROM `anggota`");
   while ($row = mysqli_fetch_object($q)){
     $data[] = $row;
   }
   echo json_encode($data);
+//  foreach ($data as d){
+//    echo $d;
+//  }
+//  var_dump($data);
 ?>
